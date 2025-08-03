bd35693f21097824fb2948e1d76c0c2ed142a35c
R-Bowes <richard.bowes@me.com>
2025-08-03 19:00:32 +0100

    Add firebase configuration

---
+++ firebase-config.js
@@ -0,0 +1,23 @@
+/* 
+ * Local Firebase configuration for the TradeStone demo.
+ *
+ * Populate this file with your own project’s configuration values.  These
+ * values are injected into `firebase-init.js` via the global
+ * `window.firebaseConfig` object.  Do not commit real API keys or service
+ * account details to version control.
+ */
+
+window.firebaseConfig = {
+  apiKey: "AIzaSyAjA_fDLdSbIW6eRFDe4oKpfdB8O4Ix4zo",
+  authDomain: "tradestone-efb30.firebaseapp.com",
+  databaseURL: "https://tradestone-efb30-default-rtdb.firebaseio.com",
+  projectId: "tradestone-efb30",
+  storageBucket: "tradestone-efb30.firebasestorage.app",
+  messagingSenderId: "761717818779",
+  appId: "1:761717818779:web:05287865a076dbfed68d3e",
+  measurementId: "G-TM9DK5H25J"
+};
+
+// To enable Firebase App Check with reCAPTCHA v3, add the following line
+// using the site key generated in the Firebase console:
+// appCheckSiteKey: "<your-recaptcha-v3-site-key>"

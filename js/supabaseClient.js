8744b6e7db7a35f46a9bd5e29ca171cf7cefa4af
R-Bowes <richard.bowes@me.com>
2025-09-14 19:40:54 +0100

    Update supabaseClient.js

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,7 +1,7 @@
 // js/supabaseClient.js
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
-const supabaseUrl = 'https://YOUR-PROJECT.ref.supabase.co';
-const supabaseKey = 'YOUR_ANON_PUBLIC_KEY'; // public key, OK in browser
+const supabaseUrl = 'https://tnvxfzmdjpsswjszwbvf.supabase.co';
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRudnhmem1kanBzc3dqc3p3YnZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTc4NzI3NjYsImV4cCI6MjA3MzQ0ODc2Nn0.rzTUVRPxybLJZ9asE04lOg-5pjFp6FZXWT3JzvshI8A'; // public key, OK in browser
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
